bfbe0ffcd62f0108884ea3832aefdaa13c4b6ee0
Thomas Quinton <cartmam1@hotmail.fr>
2021-10-24 20:39:43 +0200

    Update map.js

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,5 +1,5 @@
 // Leaflet map
-const map = L.map("map").setView([43.610769, 3.876716], 13);
+const map = L.map("map").setView([43.618, 3.877], 13);
 L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
 	attribution: "Map data &copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors, Imagery © <a href='https://www.mapbox.com/'>Mapbox</a>",
 	maxZoom: 18,
