58e4d23463cafbf434b26ad37f8574c12e0cbf76
Mattéo Legagneux <matteoo34@icloud.com>
2021-10-25 15:48:24 +0200

    Form validation

--- assets/js/form.js
+++ assets/js/form.js
@@ -3,12 +3,35 @@ const Form = {
 	communities: document.querySelector("form.communities"),
 	individuals: document.querySelector("form.individuals")*/
 },
+setError = function(input, message) {
+	input.classList.add("error");
+	console.log(message)
+},
+removeError = function(input) {
+	input.classList.remove("error")
+},
 handleForm = function(e) {
 	e.preventDefault();
-	console.log("lol")
+	let inputs = e.target.querySelectorAll("input:not(.optional)"),
+	verifiedInputs = 0;
+	for (let i = 0; i < inputs.length; i++) {
+		// Remove error on all inputs before the verification
+		removeError(inputs[i]);
+		// Input verification
+		if (inputs[i].value.length === 0) {
+			// Value length
+			setError(inputs[i], "Veuillez renseigner ce champ")
+		} else {
+			verifiedInputs++
+		}
+	}
+	if (verifiedInputs === inputs.length) e.target.submit()
 };
 
 // Form submission verification
 Form.companies.addEventListener("submit", handleForm);
 // Form.communities.addEventListener("submit", handleForm);
 // Form.individuals.addEventListener("submit", handleForm)
+document.querySelectorAll("form input").forEach(function(input) {
+	input.addEventListener("keydown", function() {removeError(this)})
+})
