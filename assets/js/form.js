e777a6b971ebba457aa5dd97413d2f9026ddabeb
Mattéo Legagneux <matteoo34@icloud.com>
2021-10-26 17:14:57 +0200

    FORM

--- assets/js/form.js
+++ assets/js/form.js
@@ -5,10 +5,12 @@ const Form = {
 },
 setError = function(input, message) {
 	input.parentNode.classList.add("error");
-	// console.log(message)
+	input.nextElementSibling.innerText = message;
+	input.nextElementSibling.style.opacity = 1
 },
 removeError = function(input) {
 	input.parentNode.classList.remove("error")
+	input.nextElementSibling.style.opacity = 0
 },
 handleForm = function(e) {
 	e.preventDefault();
@@ -21,17 +23,26 @@ handleForm = function(e) {
 		if (inputs[i].value.length === 0) {
 			// Value length
 			setError(inputs[i], "Veuillez renseigner ce champ")
-		} else {
-			verifiedInputs++
 		}
+		// Number inputs
+		else if (inputs[i].classList.contains("nb") && isNaN(inputs[i].value)) setError(inputs[i], "Veuillez entrer une valeur numérique");
+		else if (inputs[i].name === "phoneNumber" && inputs[i].value.length !== 10) setError(inputs[i], "Veuillez entrer un numéro de téléphone valide");
+		else if (inputs[i].name === "postalCode" && inputs[i].value.length !== 5) setError(inputs[i], "Veuillez entrer un code postal valide");
+		else verifiedInputs++
 	}
-	if (verifiedInputs === inputs.length) e.target.submit()
+	console.log(verifiedInputs)
+	if (verifiedInputs === inputs.length) {
+		// Form submission confirmation
+		// Hide form modal
+		e.target.parentNode.parentNode.querySelector(".modal-header .btn-close").click();
+		document.querySelector(".confirmation").style.display = "block"
+	}
 };
 
 // Form submission verification
 Form.companies.addEventListener("submit", handleForm);
 Form.communities.addEventListener("submit", handleForm);
-Form.individuals.addEventListener("submit", handleForm)
+Form.individuals.addEventListener("submit", handleForm);
 document.querySelectorAll("form input").forEach(function(input) {
 	input.addEventListener("keydown", function() {removeError(this)})
 })
