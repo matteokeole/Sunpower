c967ad5cfc0ce969a074e65c25787d545138a6df
Mattéo Legagneux <matteoo34@icloud.com>
2021-10-26 15:09:02 +0200

    logo & banner

--- assets/js/banner.js
+++ assets/js/banner.js
@@ -1,8 +1,11 @@
 // Banner color (--primary) and blur amount on scroll
-const header = document.querySelector("header");
+const header = document.querySelector("header"),
+logo = header.querySelector(".navbar-brand");
 let windowHeight = window.innerHeight,
 scrollAmount = window.scrollY / windowHeight;
+
 header.style.backgroundColor = `rgba(164, 189, 1, ${scrollAmount})`; // Set the color and blur on load
+logo.style.opacity = 0;
 
 // Event listeners
 window.addEventListener("resize", function() {windowHeight = this.innerHeight});
@@ -10,5 +13,7 @@ window.addEventListener("scroll", function() {
 	scrollAmount = this.scrollY / windowHeight;
 	header.style.backgroundColor = `rgba(164, 189, 1, ${scrollAmount})`;
 	header.style.backdropFilter = `blur(${scrollAmount * 10}px)`;
-	header.style["-webkit-backdrop-filter"] = `blur(${scrollAmount * 10}px)`
+	header.style["-webkit-backdrop-filter"] = `blur(${scrollAmount * 10}px)`;
+	// Header logo
+	logo.style.opacity = scrollAmount
 })
