a0452830b2e9a3757ecb22beb26c8eb3e0da771c
Thomas Quinton <cartmam1@hotmail.fr>
2021-10-26 13:06:19 +0200

    effet ombrieres

---
+++ assets/js/ombrieres.js
@@ -0,0 +1,29 @@
+//Fade-in-left
+const ratio = .2
+const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: .2
+  }
+  
+const handleIntersect = function(entries, observer){
+    entries.forEach(function(entry){
+        if (entry.intersectionRatio > ratio){
+            entry.target.classList.add('reveal-visible')
+            observer.unobserve(entry.target)
+        } else {
+    console.log('invisible')
+}
+})
+}
+
+  const observer = new IntersectionObserver(handleIntersect, options)
+  document.querySelectorAll('[class*="reveal1"]').forEach(function(r){
+      observer.observe(r)
+  document.querySelectorAll('[class*="reveal2"]').forEach(function(r){
+      observer.observe(r)
+  document.querySelectorAll('[class*="reveal3"]').forEach(function(r){
+      observer.observe(r)
+  document.querySelectorAll('[class*="reveal4"]').forEach(function(r){
+      observer.observe(r)
+  })})})})
